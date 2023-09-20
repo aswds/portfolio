c615945184b1d060f16f65ee10c252f2e1b76a16
aswds <sasha.iilllnitskiy@gmail.com>
2023-09-20 14:28:47 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/portfolio/",
+  plugins: [react()],
 });
 // deploy test
